de2278112a0d8fa9291ae4fd8d8dd6bc3409d945
JP Arimaa <juha-pekka.arimaa@varjo.com>
2020-11-11 20:39:52 +0200

    VE: create textures and samplers

--- Examples/VolumetricExplosion/VolumetricExplosionApp.cpp
+++ Examples/VolumetricExplosion/VolumetricExplosionApp.cpp
@@ -1,19 +1,19 @@
-﻿#include "VolumetricExplosionApp.h"
-#include <fw/Model.h>
+#include "VolumetricExplosionApp.h"
 #include <fw/Common.h>
 #include <fw/DX.h>
 #include <fw/API.h>
 #include <fw/WcharHelper.h>
 #include <fw/imgui/imgui.h>
-#include <WICTextureLoader.h>
+
 #include <DDSTextureLoader.h>
+
+#include <fstream>
 #include <vector>
 #include <iostream>
 
 namespace
 {
 float clearColor[4] = {0.0f, 0.125f, 0.3f, 1.0f};
-
 } // namespace
 
 VolumetricExplosionApp::VolumetricExplosionApp()
@@ -22,12 +22,12 @@ VolumetricExplosionApp::VolumetricExplosionApp()
 
 VolumetricExplosionApp::~VolumetricExplosionApp()
 {
+    fw::release(m_noiseVolume);
     fw::release(m_matrixBuffer);
 }
 
 bool VolumetricExplosionApp::initialize()
 {
-    //DirectX::CreateDDSTextureFromFile();
     const std::vector<D3D11_INPUT_ELEMENT_DESC> layout = {
         {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}};
 
@@ -43,7 +43,13 @@ bool VolumetricExplosionApp::initialize()
         return false;
     }
 
-    if (!createMatrixBuffer())
+    bool resourcesCreated = //
+        createNoiseTexture() && //
+        createGradientTexture() && //
+        createSamplers() && //
+        createMatrixBuffer();
+
+    if (!resourcesCreated)
     {
         return false;
     }
@@ -81,7 +87,7 @@ void VolumetricExplosionApp::render()
 
     fw::DX::context->VSSetShader(m_vertexShader.get(), nullptr, 0);
     fw::DX::context->PSSetShader(m_pixelShader.get(), nullptr, 0);
-    fw::DX::context->PSSetSamplers(0, 1, &m_linearSampler);
+    //fw::DX::context->PSSetSamplers(0, 1, &m_linearSampler);
 
     fw::DX::context->IASetInputLayout(m_vertexShader.getVertexLayout());
     fw::DX::context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -102,16 +108,115 @@ void VolumetricExplosionApp::gui()
     ImGui::Text("Hello, world!");
 }
 
+bool VolumetricExplosionApp::createNoiseTexture()
+{
+    const uint16_t noiseValueSize = 32;
+    const uint16_t noiseValueCount = noiseValueSize * noiseValueSize * noiseValueSize;
+    uint16_t noiseValues[noiseValueCount] = {0};
+    std::fstream f;
+    f.open("noise_32x32x32.dat", std::ios::in);
+    if (!f.is_open())
+    {
+        return false;
+    }
+
+    for (uint16_t i = 0; i < noiseValueCount; ++i)
+    {
+        uint16_t noiseValue;
+        f >> noiseValue;
+
+        m_maxNoiseValue = std::max(m_maxNoiseValue, noiseValue);
+        m_minNoiseValue = std::min(m_minNoiseValue, noiseValue);
+
+        noiseValues[i] = noiseValue;
+    }
+    f.close();
+
+    D3D11_TEXTURE3D_DESC texDesc{};
+    texDesc.CPUAccessFlags = 0;
+    texDesc.Height = noiseValueSize;
+    texDesc.Width = noiseValueSize;
+    texDesc.Depth = noiseValueSize;
+    texDesc.Format = DXGI_FORMAT_R16_FLOAT;
+    texDesc.MipLevels = 1;
+    texDesc.MiscFlags = 0;
+    texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+    texDesc.Usage = D3D11_USAGE_DEFAULT;
+
+    D3D11_SUBRESOURCE_DATA initialData{};
+    initialData.pSysMem = noiseValues;
+    initialData.SysMemPitch = noiseValueSize * 2;
+    initialData.SysMemSlicePitch = noiseValueSize * noiseValueSize * 2;
+
+    HRESULT hr = fw::DX::device->CreateTexture3D(&texDesc, &initialData, &m_noiseVolume);
+    if (FAILED(hr))
+    {
+        fw::printError("Failed to create noise volume", &hr);
+        return false;
+    }
+
+    hr = fw::DX::device->CreateShaderResourceView(m_noiseVolume, nullptr, &m_noiseVolumeSRV);
+    if (FAILED(hr))
+    {
+        fw::printError("Failed to create noise volume SRV", &hr);
+        return false;
+    }
+
+    return true;
+}
+
+bool VolumetricExplosionApp::createGradientTexture()
+{
+    HRESULT hr = DirectX::CreateDDSTextureFromFile(fw::DX::device, L"gradient.dds", nullptr, &m_gradientSRV);
+    if (FAILED(hr))
+    {
+        fw::printError("Failed to create gradient SRV", &hr);
+        return false;
+    }
+
+    return true;
+}
+
+bool VolumetricExplosionApp::createSamplers()
+{
+    D3D11_SAMPLER_DESC desc{};
+    desc.Filter = D3D11_FILTER_MIN_MAG_LINEAR_MIP_POINT;
+    desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+    desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+    desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+    desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+    desc.MinLOD = 0;
+    desc.MaxLOD = D3D11_FLOAT32_MAX;
+
+    HRESULT hr = fw::DX::device->CreateSamplerState(&desc, &m_samplerClamped);
+    if (FAILED(hr))
+    {
+        return false;
+        fw::printError("Failed to create sampler", &hr);
+    }
+
+    desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+    desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+    desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+    hr = fw::DX::device->CreateSamplerState(&desc, &m_samplerWrapped);
+    if (FAILED(hr))
+    {
+        return false;
+        fw::printError("Failed to create sampler", &hr);
+    }
+
+    return true;
+}
+
 bool VolumetricExplosionApp::createMatrixBuffer()
 {
-    D3D11_BUFFER_DESC bd;
-    ZeroMemory(&bd, sizeof(bd));
-    bd.ByteWidth = sizeof(DirectX::XMMATRIX) * 3;
-    bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-    bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-    bd.Usage = D3D11_USAGE_DYNAMIC;
+    D3D11_BUFFER_DESC desc{};
+    desc.ByteWidth = sizeof(DirectX::XMMATRIX) * 3;
+    desc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+    desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+    desc.Usage = D3D11_USAGE_DYNAMIC;
 
-    HRESULT hr = fw::DX::device->CreateBuffer(&bd, nullptr, &m_matrixBuffer);
+    HRESULT hr = fw::DX::device->CreateBuffer(&desc, nullptr, &m_matrixBuffer);
     if (FAILED(hr))
     {
         fw::printError("Failed to create matrix buffer", &hr);
--- Examples/VolumetricExplosion/VolumetricExplosionApp.h
+++ Examples/VolumetricExplosion/VolumetricExplosionApp.h
@@ -1,14 +1,18 @@
-﻿#pragma once
+#pragma once
 
 #include <fw/Application.h>
 #include <fw/VertexShader.h>
 #include <fw/PixelShader.h>
+#include <fw/HullShader.h>
+#include <fw/DomainShader.h>
 #include <fw/Transformation.h>
 #include <fw/PerspectiveCamera.h>
 #include <fw/CameraController.h>
 #include <fw/Common.h>
+
 #include <DirectXMath.h>
 #include <d3d11.h>
+
 #include <memory>
 
 class VolumetricExplosionApp : public fw::Application
@@ -34,10 +38,22 @@ private:
     fw::CameraController m_cameraController;
     fw::Transformation m_transformation;
     fw::VertexShader m_vertexShader;
+    fw::HullShader m_hullShader;
+    fw::DomainShader m_domainShader;
     fw::PixelShader m_pixelShader;
 
+    ID3D11Texture3D* m_noiseVolume = nullptr;
+    ID3D11ShaderResourceView* m_noiseVolumeSRV = nullptr;
+    ID3D11ShaderResourceView* m_gradientSRV = nullptr;
     ID3D11Buffer* m_matrixBuffer = nullptr;
-    ID3D11SamplerState* m_linearSampler = nullptr;
+    ID3D11SamplerState* m_samplerClamped = nullptr;
+    ID3D11SamplerState* m_samplerWrapped = nullptr;
 
+    uint16_t m_maxNoiseValue = 0;
+    uint16_t m_minNoiseValue = 0xFFFF;
+
+    bool createNoiseTexture();
+    bool createGradientTexture();
+    bool createSamplers();
     bool createMatrixBuffer();
 };
