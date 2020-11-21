18b180e957ba3f0d09eaebb30d276daaaaead559
JP Arimaa <juha-pekka.arimaa@varjo.com>
2020-11-21 21:17:30 +0200

    VE: add readme, small shader adjustments

--- Examples/VolumetricExplosion/README.md
+++ Examples/VolumetricExplosion/README.md
@@ -1,5 +1,11 @@
 # Volumetric explosion
 
-Volumetric explosion
+Animated volumetric explosion. The basic idea is to use sphere signed distance field and add noise to it, and map the displaced amount to gradient texture (the further from center, the darker it is). In addition there's an optimization done with domain shader that calculates a shell for the noised sphere.
 
-![volumetric](volumetric.png?raw=true "volumetric")
+Based on Realistic Volumetric Explosions in Games by Alex Dunn on GPU Pro 6
+
+http://gpupro.blogspot.com/2014/12/realistic-volumetric-explosions-in-games.html
+
+https://www.routledge.com/GPU-Pro-6-Advanced-Rendering-Techniques/Engel/p/book/9781482264616
+
+![ve](ve.png?raw=true "ve")
--- Examples/VolumetricExplosion/example.hlsl
+++ Examples/VolumetricExplosion/example.hlsl
@@ -88,11 +88,10 @@ float noise(float3 uvw)
 float fractalNoiseAtPositionWS(float3 posWS, uint numOctaves)
 {
     const float3 animation = noiseAnimationSpeed * time;
-
     float3 uvw = posWS * noiseScale + animation;
     float amplitude = noiseInitialAmplitude;
+    float noiseValue = 0.0f;
 
-    float noiseValue = 0;
     for (uint i = 0; i < numOctaves; ++i)
     {
         noiseValue += abs(amplitude * noise(uvw));
@@ -108,11 +107,11 @@ float sphereSDF(float3 relativePosWS, float radiusWS)
     return length(relativePosWS) - radiusWS;
 }
 
-float displacedPrimitive(float3 posWS, float3 spherePositionWS, float radiusWS, float displacementWS, uint numOctaves, out float displacementOut)
+float sphereWithNoiseSDF(float3 posWS, float3 spherePositionWS, float radiusWS, float displacementWS, uint numOctaves, out float displacementOut)
 {
-    float3 relativePosWS = posWS - spherePositionWS;
+    const float3 relativePosWS = posWS - spherePositionWS;
+    const float signedDistanceToPrimitive = sphereSDF(relativePosWS, radiusWS);
     displacementOut = fractalNoiseAtPositionWS(posWS, numOctaves);
-    float signedDistanceToPrimitive = sphereSDF(relativePosWS, radiusWS);
     return signedDistanceToPrimitive - displacementOut * displacementWS;
 }
 
@@ -128,46 +127,43 @@ struct PS_INPUT
 PS_INPUT DS(HS_CONSTANT_DATA_OUTPUT input, float2 UV : SV_DomainLocation, const OutputPatch<HS_OUTPUT, 4> quad)
 // clang-format on
 {
-    float2 posClipSpace = UV.xy * 2.0f - 1.0f;
-    float2 posClipSpaceAbs = abs(posClipSpace.xy);
-    float maxLen = max(posClipSpaceAbs.x, posClipSpaceAbs.y);
+    const float2 posClipSpace = UV.xy * 2.0f - 1.0f;
+    const float2 posClipSpaceAbs = abs(posClipSpace.xy);
+    const float maxLen = max(posClipSpaceAbs.x, posClipSpaceAbs.y);
 
-    float3 dir = normalize(float3(posClipSpace.xy, (maxLen - 1.0f)));
-    float innerRadius = explosionRadiusWS - displacementWS;
-
-    // Even though our geometry only extends around the front of the explosion volume,
-    // we can calculate the reverse side of the hull here aswell.
+    const float3 dir = normalize(float3(posClipSpace.xy, (maxLen - 1.0f)));
 
     // First get the front world space position of the hull.
-    float3 frontNormDir = dir;
+    const float3 frontNormDir = dir;
     float3 frontPosWS = mul(float4(frontNormDir, 0), viewToWorldMatrix).xyz * explosionRadiusWS + explosionPositionWS;
-    float3 frontDirWS = normalize(frontPosWS);
+    const float3 frontDirWS = normalize(frontPosWS);
     // Then perform the shrink wrapping step using sphere tracing.
+    const float innerRadius = explosionRadiusWS - displacementWS;
     float dummy;
     for (int i = 0; i < numHullSteps; ++i)
     {
-        float dist = displacedPrimitive(frontPosWS, explosionPositionWS.xyz, innerRadius, displacementWS, numHullOctaves, dummy);
+        float dist = sphereWithNoiseSDF(frontPosWS, explosionPositionWS.xyz, innerRadius, displacementWS, numHullOctaves, dummy);
         frontPosWS -= frontDirWS * dist;
     }
     frontPosWS += frontDirWS * skinThickness;
-    float4 frontPosVS = mul(float4(frontPosWS, 1), worldToViewMatrix);
-    float4 frontPosPS = mul(float4(frontPosWS, 1), worldToProjectionMatrix);
+    const float4 frontPosVS = mul(float4(frontPosWS, 1), worldToViewMatrix);
+    const float4 frontPosPS = mul(float4(frontPosWS, 1), worldToProjectionMatrix);
 
-    // Then repeat the process for the back faces.
-    float3 backNormDir = dir * float3(1, 1, -1);
+    // Even though our geometry only extends around the front of the explosion volume,
+    // we can calculate the reverse side of the hull here aswell.
+    // So repeat the process for the back faces.
+    const float3 backNormDir = dir * float3(1, 1, -1);
     float3 backPosWS = mul(float4(backNormDir, 0), viewToWorldMatrix).xyz * explosionRadiusWS + explosionPositionWS;
-    float3 backDirWS = normalize(frontPosWS);
+    const float3 backDirWS = normalize(frontPosWS);
     for (int j = 0; j < numHullSteps; ++j)
     {
-        float dist = displacedPrimitive(backPosWS, explosionPositionWS.xyz, innerRadius, displacementWS, numHullOctaves, dummy);
+        float dist = sphereWithNoiseSDF(backPosWS, explosionPositionWS.xyz, innerRadius, displacementWS, numHullOctaves, dummy);
         backPosWS -= backDirWS * dist;
     }
     backPosWS += backDirWS * skinThickness;
-    float4 backPosVS = mul(float4(backPosWS, 1), worldToViewMatrix);
-    float4 backPosPS = mul(float4(backPosWS, 1), worldToProjectionMatrix);
+    const float4 backPosVS = mul(float4(backPosWS, 1), worldToViewMatrix);
 
-    float3 relativePosWS = frontPosWS - eyePositionWS;
-    float3 rayDirectionWS = relativePosWS / dot(relativePosWS, eyeForwardWS);
+    const float3 rayDirectionWS = normalize(frontPosWS - eyePositionWS);
 
     PS_INPUT psInput;
     psInput.PosPS = frontPosPS;
@@ -180,57 +176,49 @@ float4 mapDisplacementToColor(const float displacement, const float2 uvScaleBias
 {
     float texcoord = saturate(mad(displacement, uvScaleBias.x, uvScaleBias.y));
     texcoord = 1 - (1 - texcoord) * (1 - texcoord); // Adjust
-
     float4 color = gradientTex.SampleLevel(clampedSampler, texcoord, 0);
-
     // Apply some more adjustments to the color post sample. Again, these should be made in the texture itself.
     color *= color;
     color.a = 0.5f;
-
     return color;
 }
 
 float4 sceneFunction(const float3 posWS, const float3 spherePositionWS, const float radiusWS, const float displacementWS, const float2 uvScaleBias)
 {
     float displacementOut;
-    float distance = displacedPrimitive(posWS, spherePositionWS, radiusWS, displacementWS, numOctaves, displacementOut);
+    const float distance = sphereWithNoiseSDF(posWS, spherePositionWS, radiusWS, displacementWS, numOctaves, displacementOut);
     float4 color = mapDisplacementToColor(displacementOut, uvScaleBias);
-
     // Rather than just using a binary in/out metric, we smooth the edge of the volume using a smoothstep so that we get soft edges.
-    float edgeFade = smoothstep(0.5f + edgeSoftness, 0.5f - edgeSoftness, distance);
-
+    const float edgeFade = smoothstep(0.5f + edgeSoftness, 0.5f - edgeSoftness, distance);
     return color * float4(1..xxx, edgeFade);
 }
 
-float4 Blend(const float4 src, const float4 dst)
+float4 blend(const float4 src, const float4 dst)
 {
     return mad(float4(dst.rgb, 1), mad(dst.a, -src.a, dst.a), src);
 }
 
-float4 PS(PS_INPUT input) :
-    SV_Target
+// clang-format off
+float4 PS(PS_INPUT input) : SV_Target
+// clang-format on
 {
     const float3 rayDirectionWS = input.rayDirectionWS;
-    float nearD = input.rayHitNearFar.x;
-    float farD = input.rayHitNearFar.y;
+    const float near = input.rayHitNearFar.x;
+    const float far = input.rayHitNearFar.y;
 
     float4 output;
 
-    const float3 startWS = mad(rayDirectionWS, nearD, eyePositionWS.xyz);
-    const float3 endWS = mad(rayDirectionWS, farD, eyePositionWS.xyz);
-
     const float3 stepAmountWS = rayDirectionWS * stepSizeWS;
-    const float numSteps = min(maxNumSteps, (farD - nearD) / stepSizeWS);
+    const int numSteps = min(maxNumSteps, (far - near) / stepSizeWS);
     const float innerRadius = explosionRadiusWS - displacementWS;
 
-    float3 posWS = startWS;
+    float3 posWS = mad(rayDirectionWS, near, eyePositionWS.xyz);
 
-    float stepsTaken = 0;
+    int stepsTaken = 0;
     while (stepsTaken++ < numSteps && output.a < opacity)
     {
         float4 color = sceneFunction(posWS, explosionPositionWS.xyz, innerRadius, displacementWS, uvScaleBias);
-        output = Blend(output, color);
-
+        output = blend(output, color);
         posWS += stepAmountWS;
     }
 
